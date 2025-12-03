24dbae21a725967ddeea6c699ff77a37c2897eca
stepa323 <voytov.st.vi@gmail.com>
2025-12-03 21:55:30 +0200

    changes for hosting

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ pip install -r requirements.txt
 
 cd metanit
 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
+
 python manage.py collectstatic --noinput
 
 python manage.py migrate
